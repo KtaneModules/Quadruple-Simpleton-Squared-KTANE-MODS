bda3717082907c98074cd6ce7ef215dc5f4184cf
Hawker <62062830+BlckHawker@users.noreply.github.com>
2024-09-27 01:57:08 -0400

    Add functionality

--- Assets/Simpleton Squared/QuadrupleSimpletonSquared.cs
+++ Assets/Simpleton Squared/QuadrupleSimpletonSquared.cs
@@ -9,49 +9,101 @@ using Rnd = UnityEngine.Random;
 
 public class QuadrupleSimpletonSquared : MonoBehaviour {
 
-    public KMBombInfo Bomb;
-    public KMAudio Audio;
+    KMBombInfo Bomb;
+    KMAudio Audio;
+    public AudioClip solveSound;
 
     static int ModuleIdCounter = 1;
     int ModuleId;
     private bool ModuleSolved;
 
+    List<Button> buttons;
     void Awake () {
+        Bomb = GetComponent<KMBombInfo>();
+        Audio = GetComponent<KMAudio>();
+
         ModuleId = ModuleIdCounter++;
+        buttons = new List<Button>();
 
         KMSelectable selectable = GetComponent<KMSelectable>();
-        foreach (KMSelectable qudrant in selectable.Children) {
-            qudrant.OnInteract += delegate () { Debug.Log("Press"); return false; };
+        foreach (KMSelectable qudrant in selectable.Children) 
+        {
+            foreach (KMSelectable button in qudrant.Children) 
+            {
+                Button b = new Button(button);
+                button.OnInteract += delegate () { ButtonPressed(b); return false; };
+                buttons.Add(b);
+            }
         }
 
-        //button.OnInteract += delegate () { buttonPress(); return false; };
-
+        Log("Press the buttons");
     }
 
-    void Start () {
 
+    private void ButtonPressed(Button button)
+    {
+
+        Debug.Log(button.Selectable.gameObject.name);
+        if (button.Pressed)
+        {
+            return;
+        }
+
+        KMSelectable selectable = button.Selectable;
+        selectable.AddInteractionPunch();
+        
+        //todo add audio
+        Audio.PlaySoundAtTransform(solveSound.name, transform);
+
+        //todo change button
+        selectable.transform.Find("Text").GetComponent<TextMesh>().text = "Victory!";
+
+
+        button.Pressed = true;
+
+        if (buttons.All(b => b.Pressed))
+        {
+            Solve();
+        }
     }
 
-    void Update () {
-
-    }
-
-    #pragma warning disable 414
-    private readonly string TwitchHelpMessage = @"Use !{0} to do something.";
-    #pragma warning restore 414
-
-    IEnumerator ProcessTwitchCommand (string Command) {
-        yield return null;
-    }
-
-    IEnumerator TwitchHandleForcedSolve () {
-        yield return null;
+    private void Solve()
+    {
+        Log("You pressed all the buttons");
+        ModuleSolved = true;
+        GetComponent<KMBombModule>().HandlePass();
     }
 
     private void Log(string s)
     {
         Debug.Log($"[Quadruple Simpleton Squared #{ModuleId}] {s}");
     }
+    
+
+    private class Button
+    {
+        public KMSelectable Selectable { get; private set; }
+        public bool Pressed { get; set; }
+
+        public Button(KMSelectable selectable)
+        {
+            Selectable = selectable;
+            Pressed = false;
+        }
+    }
 
 
+#pragma warning disable 414
+    private readonly string TwitchHelpMessage = @"Use !{0} to do something.";
+#pragma warning restore 414
+
+    IEnumerator ProcessTwitchCommand(string Command)
+    {
+        yield return null;
+    }
+
+    IEnumerator TwitchHandleForcedSolve()
+    {
+        yield return null;
+    }
 }
