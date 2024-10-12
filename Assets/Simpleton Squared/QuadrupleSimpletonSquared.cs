7107bb4787122045666c9cd3eae35226d049522e
Hawker <62062830+BlckHawker@users.noreply.github.com>
2024-10-12 12:04:24 -0400

    add tp support

--- Assets/Simpleton Squared/QuadrupleSimpletonSquared.cs
+++ Assets/Simpleton Squared/QuadrupleSimpletonSquared.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.RegularExpressions;
 using UnityEngine;
 
 public class QuadrupleSimpletonSquared : MonoBehaviour {
@@ -11,6 +12,7 @@ public class QuadrupleSimpletonSquared : MonoBehaviour {
 
     static int ModuleIdCounter = 1;
     int ModuleId;
+    bool ModuleSolved;
 
     List<Button> buttons;
     void Awake () {
@@ -58,6 +60,7 @@ public class QuadrupleSimpletonSquared : MonoBehaviour {
     private void Solve()
     {
         Log("You pressed all the buttons");
+        ModuleSolved = true;
         GetComponent<KMBombModule>().HandlePass();
     }
 
@@ -79,17 +82,56 @@ public class QuadrupleSimpletonSquared : MonoBehaviour {
     }
 
 
+    string[] validCoordinates = new[] {     "A1", "B1",
+                                            "A2", "B2",
+
+                                            "C1", "D1",
+                                            "C2", "D2",
+
+                                            "A3", "B3",
+                                            "A4", "B4",
+
+                                            "C3", "D3",
+                                            "C4", "D4"};
 #pragma warning disable 414
-    private readonly string TwitchHelpMessage = @"Use !{0} to do something.";
+    private readonly string TwitchHelpMessage = @"Use !{0} followed by a Battleship coordinate to press that button. You can chain buttons presses with a "","" between the coordinates";
 #pragma warning restore 414
 
     IEnumerator ProcessTwitchCommand(string Command)
     {
+        Command = Command.ToUpper().Trim();
         yield return null;
+        
+
+        Match match = Regex.Match(Command, @"^([A-D][1-4],?\s?)+$", RegexOptions.CultureInvariant | RegexOptions.IgnoreCase);
+
+        if (match.Success) 
+        {
+            string[] coordinates = match.Groups[0].Value.Split(',').Select(coor => coor.Trim()).ToArray();
+
+            foreach (string coor in coordinates)
+            {
+                Debug.Log(coor);
+                int ix = Array.IndexOf(validCoordinates, coor);
+                buttons[ix].Selectable.OnInteract();
+                yield return new WaitForSeconds(.1f);
+            }
+        }
+
+        else
+        {
+            yield return $"sendtochaterror Could not recongize that command";
+            yield break;
+        }
+
     }
 
     IEnumerator TwitchHandleForcedSolve()
     {
-        yield return null;
+        yield return ProcessTwitchCommand(string.Join(",", validCoordinates));
+        while (!ModuleSolved)
+        {
+            yield return null;
+        }
     }
 }
